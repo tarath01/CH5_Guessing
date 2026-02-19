ac323613d395a1b27911e7980decfc1de932ff27
tarath01 <epstrath01@gmail.com>
2026-02-19 15:15:08 -0600

    color switch

--- guess.js
+++ guess.js
@@ -1,6 +1,7 @@
 "use strict";
 
-// global variables 
+console.log(getRandomInt());
+// global variables
 let randomNum = 0;
 let tries = 0;
 
@@ -14,12 +15,13 @@ const getRandomInt = (max = 100) => {
 // event handler functions
 const guessClick = () => {
     const guess = parseInt(document.querySelector("#number").value);
+    const distance = 100;
 
     let message = "";
     if (isNaN(guess)) {
         message = "Not a valid number. Please enter a valid number."
-    } else if (guess < 1 || guess > 10) {
-        message = "Invalid number. Enter a number between 1 and 10.";
+    } else if (guess < 1 || guess > 100) {
+        message = "Invalid number. Enter a number between 1 and 100.";
     } else if (guess < randomNum) {
         message = "Too small. Try again.";
         tries++;
@@ -31,11 +33,47 @@ const guessClick = () => {
         const lastWord = (tries === 1) ? "try" : "tries";
         message = `You guessed it in ${tries} ${lastWord}!`;
     }
+    switch (true) {
+        case (distance === 0):
+            const lastWord = (tries === 1) ? "try" : "tries";
+            message = `Fire! You guessed it in ${tries} ${lastWord}!`;
+            color = "green";
+            updateBestScore();
+            break;
+        case(distance <= 5):
+            message = 'Hot! (Within 5)';
+            color = "red";
+            break;
+        case (distance <= 10):
+            message = 'Warmer (Within 10)';
+            color = "orangered";
+            break;
+        case (distance <= 20):
+            message = 'Warm (Within 20)';
+            color = "orange";
+            break;
+        case (distance >= 30):
+            message = 'Cold (Within 30)';
+            color = "lightblue";
+            break;
+        case (distance >= 40):
+            message = 'Colder (Within 40)';
+            color = "blue";
+            break;
+        case (distance >= 100):
+            message = 'Otherwise Freezing (Way Off)';
+            color = "darkblue";
+    }
     document.querySelector("#message").textContent = message;
 };
 
+const updateBestScore = () => {}
+
+
+
+
 const playAgainClick = () => {
-    randomNum = getRandomInt(10);
+    randomNum = getRandomInt(100);
     tries = 0;
     document.querySelector("#number").value = "";
     document.querySelector("#message").textContent = "";
@@ -48,4 +86,11 @@ document.addEventListener("DOMContentLoaded", () => {
         "click", guessClick);
     document.querySelector("#play_again").addEventListener(
         "click", playAgainClick);
+
+    //Enable "Enter" Key
+    document.querySelector("#number").addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+            guessClick();
+        }
+    });
 });
