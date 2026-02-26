760f47dafa104a78fed79b013be447b447f33dec
tarath01 <epstrath01@gmail.com>
2026-02-25 19:58:40 -0600

    updated readme and included history

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 <b>Table of Contents</b>
 - [Summary](#summary)
 - [Screenshots](#screenshots)
-  -  [Freezing](#freezing)
   - [Hot](#hot)
+  - [Warmer](#warmer)
+  - [Warm](#warm)
+  - [Cold](#cold)
+  - [Colder](#colder)
+  - [Freezing](#freezing)
   - [Fire](#fire)
 - [Maintainers](#maintainers)
 
@@ -17,12 +21,20 @@ This Guessing Game is a fun hot/cold game that will signal when your far or clos
 number!
 
 ## ScreenShots
-### Freezing
-![Freezing]()
 ### Hot
-![Hot]()
+![Hot](assets/hot.png)
+### Warmer
+![Warmer](assets/warmer.png)
+### Warm
+![Warm](assets/warm.png)
+### Cold
+![Cold](assets/cold.png)
+### Colder
+![Colder](assets/colder.png)
+### Freezing
+![Freezing](assets/freezing.png)
 ### Fire
-![Fire]()
+![Fire](assets/bestscore.png)
 ### Maintainers
 [@tarath01](https://github.com/tarath01) Taylor Rath <br>
 
--- guess.js
+++ guess.js
@@ -1,9 +1,22 @@
+/*
+==========================================================
+Guessing Game (1 - 100)
+Author: Taylor Rath
+Date: 02/25/2026
+GitHub: https://github.com/tarath01/CH5_Guessing
+
+Program Summary: This program generates a random number
+between 1 and 100. The user is to attempt to guess the number. The program provides
+feedback in a color code based on how close your guess is.
+ */
+
+
 "use strict";
 
-console.log(getRandomInt);
 // global variables
 let randomNum = 0;
 let tries = 0;
+let bestScore = 0;
 
 // helper function
 const getRandomInt = (max = 100) => {
@@ -15,9 +28,11 @@ const getRandomInt = (max = 100) => {
 // event handler functions
 const guessClick = () => {
     const guess = parseInt(document.querySelector("#number").value);
-    const distance = 100;
+    const distance = Math.abs(randomNum - guess);
 
+    let color = "black";
     let message = "";
+
     if (isNaN(guess)) {
         message = "Not a valid number. Please enter a valid number."
     } else if (guess < 1 || guess > 100) {
@@ -33,51 +48,72 @@ const guessClick = () => {
         const lastWord = (tries === 1) ? "try" : "tries";
         message = `You guessed it in ${tries} ${lastWord}!`;
     }
-    let color = "black";
+    // color messages based on distance
     switch (true) {
         case (distance === 0):
             const lastWord = (tries === 1) ? "try" : "tries";
-            message = 'Fire! You guessed it in ${tries} ${lastWord}!`;
+            message = `Fire! You guessed it in ${tries} ${lastWord}!`;
             color = "green";
             updateBestScore();
             break;
         case(distance <= 5):
-            message = 'Hot! (Within 5)';
+            message = "Hot! (Within 5)";
             color = "red";
             break;
         case (distance <= 10):
-            message = 'Warmer (Within 10)';
+            message = "Warmer (Within 10)";
             color = "orangered";
             break;
         case (distance <= 20):
-            message = 'Warm (Within 20)';
+            message = "Warm (Within 20)";
             color = "orange";
             break;
         case (distance <= 30):
-            message = 'Cold (Within 30)';
+            message = "Cold (Within 30)";
             color = "lightblue";
             break;
         case (distance <= 40):
-            message = 'Colder (Within 40)';
+            message = "Colder (Within 40)";
             color = "blue";
             break;
         default:
-            message = 'Otherwise Freezing (Way Off)';
+            message = "Otherwise Freezing (Way Off)";
             color = "darkblue";
+            break;
     }
+
+    //Update History & guess history
+    const historyEl = document.querySelector("#history")
+    historyEl.innerHTML += `Guess ${tries}: ${guess} - ${message}<br>`;
+
+    //color
     const messageEl = document.querySelector("#message")
-    messageEl.style.color = color;
+    messageEl.style.color = color; // set color
     messageEl.textContent = message;
+
+    // set focus back to input and clear
+    const guessInput = document.querySelector("#number");
+    guessInput.value = ""; //clear input
+    guessInput.focus(); // set focus back
 };
 
-const updateBestScore = () => {
 
-}
+//best score
+const updateBestScore = () => {
+    if (bestScore === 0 || tries < bestScore) { // used AI on this line to help give me idea on how to start
+        bestScore = tries;
+        document.querySelector("#best_score").textContent = bestScore;
+    }
+};
+
 const playAgainClick = () => {
     randomNum = getRandomInt(100);
+
+    console.log(randomNum); // console log for random # output
     tries = 0;
     document.querySelector("#number").value = "";
     document.querySelector("#message").textContent = "";
+    document.querySelector("#history").innerHTML = "";
 };
 
 document.addEventListener("DOMContentLoaded", () => {
